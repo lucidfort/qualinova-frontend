5451f258fa4eb10c2c18296523eb92048a099582
Epiphanus <onyesoepiphanus@gmail.com>
2025-03-22 20:47:15 +0100

    removed the component initially placed for previewing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import ThirdStepForm from "@/components/organisms/CertificateCreation/ThirdStepForm";
-
 export default function Home() {
   return (
-    <div className="w-full min-h-screen flex items-center justify-center py-6">
-      <ThirdStepForm />
+    <div >
+      Base Page
     </div>
   );
 }
